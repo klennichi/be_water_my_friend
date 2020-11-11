8ba00b355a9ab9aeb212cd202fe5ceb2bb4004ea
klennichi <meandrickkleinem@gmail.com>
2020-11-11 21:31:39 +0800

    Initial commit

---
+++ app/src/main/java/meandrick/martin/front_page_risaikuru/MainActivity.java
@@ -0,0 +1,18 @@
+package meandrick.martin.front_page_risaikuru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/meandrick/martin/front_page_risaikuru/MainActivity2.java
@@ -0,0 +1,14 @@
+package meandrick.martin.front_page_risaikuru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
