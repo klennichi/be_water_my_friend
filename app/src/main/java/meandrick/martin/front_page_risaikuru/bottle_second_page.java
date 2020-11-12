56806f18ec286399e6cd421306450c0f8a7150da
klennichi <meandrickkleinem@gmail.com>
2020-11-12 23:11:47 +0800

    #blessed

---
+++ app/src/main/java/meandrick/martin/front_page_risaikuru/bottle_second_page.java
@@ -0,0 +1,14 @@
+package meandrick.martin.front_page_risaikuru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bottle_second_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottle_second_page);
+    }
+}
